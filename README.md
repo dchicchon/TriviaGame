af251bfcf319b73e95ec25d1efd8c5273aa82d3c
blueDogs <danielchicchon@gmail.com>
2019-06-01 16:09:45 -0700

    changed font in style.css, imported the font file. Added questions from questions found online.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TriviaGame
+
+- [Space Font](https://www.1001fonts.com/science-fiction+space-age-fonts.html)
+- [Space Trivia Questions](https://conversationstartersworld.com/space-trivia-questions/)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,16 @@ $( document ).ready(function() {
     // Variables //
     var questions = [
         questionOne = {
-            'q': 'What is my favorite color?',
+            'q': '1. What type of galaxy is the most common in the universe?',
             'a' : {
-                'Blue': true,
+                'Elliptical': true,
                 'Yellow': false,
                 'Green': false,
                 'Purple': false
             }
         },
         questionTwo = {
-            'q': 'What is your favorite color?',
+            'q': '2. What is the coldest place in the universe? (Hint: It’s a nebula.)',
             'a': {
                 'Blue': false,
                 'Yellow': false,
@@ -21,7 +21,7 @@ $( document ).ready(function() {
             }
         },
         questionThree = {
-            'q': 'What is her favorite color?',
+            'q': '3. The hottest place in the universe is located in which constellation?',
             'a': {
                 'Blue': false,
                 'Yellow': false,
@@ -31,7 +31,73 @@ $( document ).ready(function() {
         },
 
         questionFour = {
-            'q': 'Where is the ocean',
+            'q': '4. How old is the universe in years? (Plus or minus 1 billion years.)',
+            'a': {
+                'Blue': false,
+                'Yellow': false,
+                'Green': false,
+                'purple': true
+            }
+                  
+        },
+
+        questionFive = {
+            'q': '5. What percent of the universe is dark matter? (Plus or minus 2%.)',
+            'a': {
+                'Blue': false,
+                'Yellow': false,
+                'Green': false,
+                'purple': true
+            }
+                  
+        },
+
+        questionSix = {
+            'q': '6. How many planets are in the Solar System?',
+            'a': {
+                'Blue': false,
+                'Yellow': false,
+                'Green': false,
+                'purple': true
+            }
+                  
+        },
+
+        questionSeven = {
+            'q': '7. What is the largest planet in our solar system?',
+            'a': {
+                'Blue': false,
+                'Yellow': false,
+                'Green': false,
+                'purple': true
+            }
+                  
+        },
+
+        questionEight = {
+            'q': '8. What is the smallest planet in our solar system?',
+            'a': {
+                'Blue': false,
+                'Yellow': false,
+                'Green': false,
+                'purple': true
+            }
+                  
+        },
+
+        questionNine = {
+            'q': '9. What is the most common type of star found in the Milky Way?',
+            'a': {
+                'Blue': false,
+                'Yellow': false,
+                'Green': false,
+                'purple': true
+            }
+                  
+        },
+
+        questionTen = {
+            'q': '10. What is the largest type of star in the universe?',
             'a': {
                 'Blue': false,
                 'Yellow': false,
@@ -40,7 +106,11 @@ $( document ).ready(function() {
             }
                   
         }
+
+
+
     ]
+
 
     // This for loop goes through the list of questions
     for (i=0; i<questions.length; i++) {
@@ -52,8 +122,10 @@ $( document ).ready(function() {
         // At element id #question, we append a new element <h3> with the questionName.
 
         $(questAnswerDiv).append('<h3>'+questionName+ '</h3>')
+
         
         var answerName = Object.keys(questions[i].a)
+
         $(questAnswerDiv).append('<p>'+ answerName[0] + '</p>')
         $(questAnswerDiv).append('<p>'+ answerName[1] + '</p>')
         $(questAnswerDiv).append('<p>'+ answerName[2] + '</p>')
@@ -62,6 +134,10 @@ $( document ).ready(function() {
         $("main").append(questAnswerDiv);
         
     }
+
+    // Music level property //
+    $('#music').volume = 0.2
+    
 
     
 
