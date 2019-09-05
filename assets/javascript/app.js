5038edc9b6c22348f40cff9b7ca5a52358cfb161
dchicchon <danielchicchon@gmail.com>
2019-09-05 13:22:37 -0700

    add proper callback function to setInterval

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,180 +1,222 @@
-$( document ).ready(function() {
+// PSEUDOCODE
+// I want a list of questions that I will go through based on a series of clicks and timers
+// I want to display one question with its possible answers at one time.
+// There will be 15 seconds to answer each question.
+// If the 15 seconds are up without user input, the number of incorrect answers will increment and we will show the correct answer
+// The user will have to click 'a' to continue on to the next question.
+// If the user clicks on the correct answer before the time is up, I will display a success message and wait for the user to type 'a' to continue on to the next question
+// Once all of the questions have been asked, we will be brought into a final score screen to show the user their score. I will also give them the option to play the game again if they would like
 
-    
-    // Variables //
+// Stretch Goals
+// Add a database that will keep track of a leaderboard for the users score. E.g. Enter your name!
+// Have a larger list of space questions that will be randomized for each playthrough
+// Add additional CSS that will include a custom space background instead of playing a gif in the background
+
+
+$(document).ready(function () {
+
+
+    // List of questions
+    // Structure ===========
+    // key of question = 1 Value
+    // Key of answers = 4 Values
+    // Key of correct answer = 1 Value
     var questions = [
+
         questionOne = {
             'q': '1. How many large groups of rings does Saturn have?',
             'a': ['Four', 'Seven', 'Nine', 'Five'],
-            'correct': 'Four' 
-        }, 
+            'correct': 'Four'
+        },
 
         questionTwo = {
             'q': '2. How much of the Earth is covered in water?',
-            'a': ['75%','85%','90%','50%'],
+            'a': ['75%', '85%', '90%', '50%'],
             'correct': '75%'
         },
 
         questionThree = {
             'q': '3. What is the sun?',
-            'a': ['Planet','Star','Comet','Nebula'],
+            'a': ['Planet', 'Star', 'Comet', 'Nebula'],
             'correct': 'Star'
         },
 
         questionFour = {
             'q': '4. What is the furthest planet from the sun?',
-            'a': ['Uranus','Neptune','Saturn','Jupiter'],
+            'a': ['Uranus', 'Neptune', 'Saturn', 'Jupiter'],
             'correct': 'Neptune'
         },
 
         questionFive = {
             'q': 'What is the hottest planet in the solar system?',
-            'a': ['Mercury','Venus','Mars','Earth'],
+            'a': ['Mercury', 'Venus', 'Mars', 'Earth'],
             'correct': 'Mercury'
         },
 
         questionSix = {
             'q': '6. How many planets are in the Solar System?',
-            'a': ['Eight','nine','ten','seven'],
+            'a': ['Eight', 'nine', 'ten', 'seven'],
             'correct': 'Eight'
         },
 
         questionSeven = {
             'q': '7. What is the largest planet in our solar system?',
-            'a': ['Jupiter','Neptune','Mercury','Saturn'],
+            'a': ['Jupiter', 'Neptune', 'Mercury', 'Saturn'],
             'correct': 'Jupiter'
         },
 
         questionEight = {
             'q': '8. What is the smallest planet in our solar system?',
-            'a': ['Mercury','Venus','Mars','Earth'],
-            'correct': 'Mercury'         
+            'a': ['Mercury', 'Venus', 'Mars', 'Earth'],
+            'correct': 'Mercury'
         },
 
         questionNine = {
             'q': '9. What is the most abundant element found in the universe?',
-            'a': ['Helium','Carbon','Hydrogen','Uranium'],
+            'a': ['Helium', 'Carbon', 'Hydrogen', 'Uranium'],
             'correct': 'Hydrogen'
-                  
+
         },
 
         questionTen = {
             'q': '10. Which planet has the famous Big Red Dot?',
-            'a': ['Mars','Jupiter','Saturn','Uranus'],
-            'correct': 'Jupiter'   
+            'a': ['Mars', 'Jupiter', 'Saturn', 'Uranus'],
+            'correct': 'Jupiter'
         }
     ]
+
+    // Number of Incorrect Answers
     var incorrect = 0;
+
+    // Number of Correct Answers
     var correct = 0;
+
+    // Current Question Number
     var currentQuest = 0;
-    
+
 
     // Define Functions
 
-   
 
-    // function decrement() {
-    //     var seconds = 15;
-    //     seconds--;
-    //     $('#timer').html('<h2>' + seconds + '</h2>')
-    //     if (seconds === 0) {
-            // $(quest).append('<p>Times up! The correct answer was'+ answer + '. Press "a" to continue.</p>')
-            // document.onkeyup = function(event) {
-            //     if (event.key === 'a') {
-            //         askQuestion()
-    //             }
-    //         }
-    //     }
-    // }
 
+
+
+    // What does this function do? It looks a little gnarly. I should probably split this one up into multiple functions or just hardcode it.
     function askQuestion() {
+
+        // This will empty the 'main' element on the page
         $('main').empty()
-        
+
+        // If the current question does not equal the questions array length, we will continue
         if (currentQuest != questions.length) {
-            // var seconds = 15
-            
-            
-            // var current = setInterval(decrement,1000)
-
-            // var timer = $('<div id = timer>'+ current + '</div>')
-
-            // $('body').append(timer)
-
+            // Here we have our div for the answer to the question
             var questionAnswerDiv = $('<div class = questAnswer>')
-            var answer = questions[currentQuest].correct
-            var quest = $('<h3>'+questions[currentQuest].q+'</h3>')
 
-            function timeUp() {
-                $('main').empty()
-                $('main').append('<p>Times up! The correct answer was '+ answer + '. Press "a" to continue.</p>')
-                currentQuest++
-                document.onkeyup = function(event) {
-                    if (event.key === 'a') {
-                        askQuestion()
+            // Here we grab the answer at the index of the question
+            var answer = questions[currentQuest].correct
+
+            // Here we grab the question based on the value of currentQuest
+            var quest = $('<h3>' + questions[currentQuest].q + '</h3>')
+
+            // Timer
+            function countdown() {
+                console.log(timer)
+                timer--
+                if (timer === 0) {
+                    clearInterval(incrementer)
+                    $('main').empty()
+                    $('main').append('<p>Times up! The correct answer was ' + answer + '. Press "a" to continue.</p>')
+                    currentQuest++
+                    document.onkeyup = function (event) {
+                        if (event.key === 'a') {
+                            askQuestion()
+                        }
                     }
-                }    
+                }
+
             }
 
-            setInterval(timeUp, 15000)
-            
-            for (i=0;i<questions[currentQuest].a.length;i++) {
-                var option = $('<button class = "selection">'+questions[currentQuest].a[i]+'</button>')
-                $(option).attr('data-value',questions[currentQuest].a[i])
+            var timer = 15;
+
+
+            // This is our countdown
+            var incrementer = setInterval(countdown, 1000 * 1)
+
+            // This is happening throughout the incrementer as a callback
+
+            // Set interval for 15 seconds with function countdown
+
+            for (i = 0; i < questions[currentQuest].a.length; i++) {
+                var option = $('<button class = "selection">' + questions[currentQuest].a[i] + '</button>')
+                $(option).attr('data-value', questions[currentQuest].a[i])
                 $(quest).append(option)
             }
 
             $(questionAnswerDiv).append(quest)
             $('main').append(questionAnswerDiv)
 
-            $('.selection').on('click', function() {
+            $('.selection').on('click', function () {
+
+                // Whatever is selected, we store this in a variable
                 var selected = $(this).attr("data-value")
-                if(selected === answer) {
+
+                // This will occur if the user selects the right answer
+                if (selected === answer) {
+
+                    // Here we clear the timer
+                    clearInterval(incrementer)
+
                     $(quest).append('<p = > You Guessed Correct! Press "a" to continue. </p>')
                     correct = correct + 1
                     currentQuest++
-                    document.onkeyup = function(event) {
+                    document.onkeyup = function (event) {
                         if (event.key === 'a') {
                             askQuestion()
                         }
                     }
                 }
                 else {
-                    $(quest).append('<p> You Guessed Incorrect! The answer was '+ answer +'. Press "a" to continue</p>')
+                    clearInterval(incrementer)
+                    $(quest).append('<p> You Guessed Incorrect! The answer was ' + answer + '. Press "a" to continue</p>')
                     incorrect = incorrect + 1
                     currentQuest++
-                    document.onkeyup = function(event) {
+                    document.onkeyup = function (event) {
                         if (event.key === 'a') {
                             askQuestion()
                         }
-                    }      
+                    }
                 }
             })
         }
         else {
-            $('main').append('<h3>Thank you for taking the quiz! You got '+ correct+' out of '+ questions.length+' questions correct.</h3>')
-        } 
+            $('main').append('<h3>Thank you for taking the quiz! You got ' + correct + ' out of ' + questions.length + ' questions correct.</h3>')
+        }
     }
-    
+
     $('main').hide()
     var intro = $('<div class = "intro">')
-    $(intro).append('<p>' + 'Welcome to Space Trivia! Here you will test your knowledge of the world beyond our own. You will have 15 seconds answer each question presented to you. Good luck!'+ '</p>')
+    $(intro).append('<p>' + 'Welcome to Space Trivia! Here you will test your knowledge of the Universe that we inhabit. You will have 15 seconds answer each question presented to you. Good luck!' + '</p>')
     $(intro).append('<button id = "start">Click to Start!</button>')
     $('section').append(intro)
-    
 
-    // This for loop goes through the list of questions
 
-    $('#start').on('click',function() {
+    // When this button is clicked, the game should start
+    $('#start').on('click', function () {
+        // Here we hide the section element
         $('section').hide()
+
+        // We run the function askQuestion which will begin asking questions
         askQuestion()
+
+        // Here we will now show the main div
         $('main').show()
     })
-    
 
-    // Music level property //
+
+    // Music level property
     $('#music').volume = 0.2
-    
 
-    
+
+
 
 });
